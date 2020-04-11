b0956174e13ebc324bf082ffdf5bfb4d6da40c3e
Vadym Nedoinov <vadim.nedoynov@gmail.com>
2020-04-11 13:37:38 +0300

    Docker, postgress connection, debug toolbar are added

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.7.6
+RUN apt-get update && apt-get install -y \
+    python-dev \
+    python-setuptools \
+    && apt-get clean
+
+WORKDIR /srv/project
+
+COPY requirements.txt /tmp/requirements.txt
+
+RUN pip install --upgrade pip
+RUN pip install -r /tmp/requirements.txt
---
+++ src/test_app/settings_local.dev.py
@@ -0,0 +1,18 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'as3(&&(@yre&@0)li6oyn8-i0*bk^5(t-=r6yrz5i_=bb4ge^d'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
